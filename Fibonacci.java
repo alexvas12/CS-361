0428609d82cf4b2d2386c474a3195bf4a84772fc
alexvas12 <124331545+alexvas12@users.noreply.github.com>
2024-10-07 19:06:42 -0400

    Question 5 from homework 2

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci{
+    public static int fib(int n){
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else{
+            return fib(n-1) + fib(n-2);
+        }
+    }
+    public static void main(String[]args){
+        System.out.println(fib(5)); //5
+        System.out.println(fib(40));//
+    }
+}
